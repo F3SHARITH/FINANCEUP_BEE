72633e07ecc4494ad6be7e4cdab74ba4e6499a5f
F3SHARITH <dahiana.bermudez2007@gmail.com>
2026-05-25 14:43:25 -0500

    aaaa

--- educacion/database/scripts/20260520_160006_educacion_down.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS "educacion"."progreso_leccion";
-DROP TABLE IF EXISTS "educacion"."progreso_educativo";
-DROP TABLE IF EXISTS "educacion"."leccion";
-DROP TABLE IF EXISTS "educacion"."contenido";
-DROP TABLE IF EXISTS "educacion"."modulo_educativo";
-
-DROP SCHEMA IF EXISTS "educacion";
-DROP FUNCTION IF EXISTS fn_update_fecha_modificacion();
