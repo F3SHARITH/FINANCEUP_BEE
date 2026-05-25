9cf72b42de4e08675d8d04e8d214187f541edcbd
F3SHARITH <dahiana.bermudez2007@gmail.com>
2026-05-25 15:23:26 -0500

    se realizan cambios

--- negocio/FINANCEUP_BEE/models/producto_crediticio.go
+++ negocio/FINANCEUP_BEE/models/producto_crediticio.go
@@ -60,7 +60,7 @@ func GetProductoCrediticioById(id int) (v *ProductoCrediticio, err error) {
 func GetAllProductoCrediticio(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(ProductoCrediticio))
+	qs := o.QueryTable(new(ProductoCrediticio)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
